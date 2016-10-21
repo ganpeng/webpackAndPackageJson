2023258733c15a90f490c3323badf3c5bf5b0e5e
louisGan <louisGan1988@gmail.com>
2016-10-21 16:46:22 +0800

    new

---
+++ server.js
@@ -0,0 +1,44 @@
+var path = require('path');
+var express = require('express');
+var webpack = require('webpack');
+var config = require('./webpack.config');
+
+var app = express();
+var compiler = webpack(config);
+
+app.use(express.static(path.join(__dirname, '/build')))
+//use in webpack development mode
+app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo: false,
+    publicPath: config.output.publicPath
+}));
+app.use(require('webpack-hot-middleware')(compiler));
+
+//use in webpack production mode
+//app.use(express.static(__dirname));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build/index.html'));
+});
+
+app.get('/user', function(req, res) {
+  res.json({
+    ok : 0,
+    data : {
+      user : {
+        name : 'louisGan',
+        age : 28
+      }
+    }
+  })
+})
+
+
+app.listen(3000, 'localhost', function(err) {
+    if (err) {
+        console.log(err);
+        return;
+    }
+
+    console.log('Listening at http://localhost:3000');
+});

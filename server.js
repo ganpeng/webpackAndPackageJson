4d7127d13b483b71ce7ddccf86f616ac68557643
louisGan <louisGan1988@gmail.com>
2016-10-24 17:13:10 +0800

    修改

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ app.use(express.static(path.join(__dirname, '/build')))
 //use in webpack development mode
 app.use(require('webpack-dev-middleware')(compiler, {
     noInfo: false,
-    publicPath: config.output.publicPath
+    publicPath: config.output.publicPath,
+    hot : true
 }));
 app.use(require('webpack-hot-middleware')(compiler));
 
@@ -20,7 +21,6 @@ app.use(require('webpack-hot-middleware')(compiler));
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'build/index.html'));
 });
-
 
 app.listen(3000, 'localhost', function(err) {
     if (err) {
--- webpack.config.js
+++ webpack.config.js
@@ -19,74 +19,78 @@ const AUTOPREFIXER_BROWSERS = [
 
 
 function entry() {
-  // return {
-  //   js: __dirname + "/client/index.js",
-  // }
-  return [
-    "webpack-hot-middleware/client?reload=true",
-    __dirname + "/client/index.js"
-  ]
+  return {
+    js: __dirname + "/client/index.js",
+    vendor: [
+      'react',
+      'react-redux',
+      'react-router',
+      'redux'
+    ]
+  }
 }
 
 function output() {
   return {
     path: __dirname + "/build",
-    filename: "[name]-[hash].js"
+    filename: "[name]-[hash].js",
+    publicPath: '/'
   }
 }
 
 
 function loaders() {
-  return [
-    {
-      test: /\.txt$/,
-      loader: 'raw',
+  return [{
+    test: /\.txt$/,
+    loader: 'raw',
+  }, {
+    test: /\.json$/,
+    loader: "json"
+  }, {
+    test: /\.(png|jpg|jpeg|gif|svg|woff|woff2)$/,
+    loader: 'url-loader',
+    query: {
+      name: DEBUG ? '[path][name].[ext]?[hash]' : '[hash].[ext]',
+      limit: 10000,
     },
-    {
-      test: /\.json$/,
-      loader: "json"
+  }, {
+    test: /\.(eot|ttf|wav|mp3)$/,
+    loader: 'file-loader',
+    query: {
+      name: DEBUG ? '[path][name].[ext]?[hash]' : '[hash].[ext]',
     },
-    {
-      test: /\.(png|jpg|jpeg|gif|svg|woff|woff2)$/,
-      loader: 'url-loader',
-      query: {
-        name: DEBUG ? '[path][name].[ext]?[hash]' : '[hash].[ext]',
-        limit: 10000,
-      },
-    },
-    {
-      test: /\.(eot|ttf|wav|mp3)$/,
-      loader: 'file-loader',
-      query: {
-        name: DEBUG ? '[path][name].[ext]?[hash]' : '[hash].[ext]',
-      },
-    },
-    {
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: 'babel'
-    },
-    {
-      test: /\.css$/,
-      loader: ExtractTextPlugin.extract('style', 'css?modules!postcss?pack=default')
+  }, {
+    test: /\.js$/,
+    exclude: /node_modules/,
+    loader: 'babel',
+    query: {
+      cacheDirectory: true,
+      plugins: ['transform-runtime'],
+      presets: ['es2015', 'react', 'stage-0']
     }
-  ]
+  }, {
+    test: /\.css$/,
+    // loader: ExtractTextPlugin.extract('style', 'css?modules!postcss?pack=default')   // css的class为随机字符串
+    loader: ExtractTextPlugin.extract('style', `css!postcss?pack=default`) // css的class为用户定义的字符串
+  }]
 }
 
 function plugins() {
   return [
     new webpack.DefinePlugin({
-      'process.env': { NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'production') }
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'production')
+      }
     }),
     new HtmlWebpackPlugin({
       template: __dirname + "/client/index.html"
     }),
-    // new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.bundle.js'),
+    new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.bundle.js'),
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.optimize.UglifyJsPlugin({
       output: {
-        comments: false,  // remove all comments
+        comments: false, // remove all comments
       },
       compress: {
         warnings: false
@@ -110,7 +114,9 @@ module.exports = {
     return {
       default: [
         // Transfer @import rule by inlining content, e.g. @import 'normalize.css'
-        require('postcss-partial-import')({ addDependencyTo: bundler }),
+        require('postcss-partial-import')({
+          addDependencyTo: bundler
+        }),
         // W3C variables, e.g. :root { --color: red; } div { background: var(--color); }
         // https://github.com/postcss/postcss-custom-properties
         require('postcss-custom-properties')(),
@@ -149,7 +155,9 @@ module.exports = {
         require('postcss-flexbugs-fixes')(),
         // Add vendor prefixes to CSS rules using values from caniuse.com
         // https://github.com/postcss/autoprefixer
-        require('autoprefixer')({ browsers: AUTOPREFIXER_BROWSERS }),
+        require('autoprefixer')({
+          browsers: AUTOPREFIXER_BROWSERS
+        }),
         require('postcss-mixins')(),
         require('postcss-advanced-variables')(),
         require('postcss-nested')(),
@@ -166,8 +174,4 @@ module.exports = {
     modulesDirectories: ['node_modules'],
     extensions: ['', '.webpack.js', '.web.js', '.js', '.jsx', '.json'],
   }
-  // devServer: {
-  //   contentBase: './client',
-  //   hot: true
-  // }
 }
